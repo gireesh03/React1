699cc743d90265408cfed47536bb7a003b9e129b
gireesh03 <gireeshnani03@gmail.com>
2023-10-15 12:36:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Box } from './components/Task';
+
+function App() {
+  return (
+    <div className="App">
+      <Box/>
+    </div>
+  );
+}
+
+export default App;
